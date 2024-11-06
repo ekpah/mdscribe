a785327b916a7c01a5086890867bfef936ad90c6
ekpah <nils.hapke@we-mail.de>
2024-11-06 17:30:52 +0100

    try to fix updatedAt error

--- app/templates/[id]/_components/NavActions.tsx
+++ app/templates/[id]/_components/NavActions.tsx
@@ -60,7 +60,7 @@ export function NavActions({
     <div className="flex items-center gap-2 text-sm">
       <div className="hidden font-medium text-muted-foreground md:inline-block">
         Zuletzt bearbeitet am{" "}
-        {template?.updatedAt.toLocaleDateString("de-DE", {
+        {template?.updatedAt?.toLocaleDateString("de-DE", {
           year: "numeric",
           month: "long",
           day: "numeric",
