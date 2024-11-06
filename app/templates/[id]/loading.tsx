926920be0e3fb80fc171433477830a1b98f147eb
ekpah <nils.hapke@we-mail.de>
2024-11-06 17:24:14 +0100

    fix type error

--- app/templates/[id]/loading.tsx
+++ app/templates/[id]/loading.tsx
@@ -1,9 +1,11 @@
+import { Prisma } from "@prisma/client";
 import ContentSection from "./_components/ContentSection";
 
 export default function Loading({ params }) {
   // You can add any UI inside Loading, including a Skeleton.
   return (
     <ContentSection
+      template={{} as Prisma.TemplateGetPayload<{}>}
       inputTags={JSON.stringify({ infoTags: [], switchTags: [] })}
       note={JSON.stringify("Loading...")}
     />
