be0646cbbab4f809e69b64ef1234ed7f70265240
ekpah <nils.hapke@we-mail.de>
2024-11-07 14:43:55 +0100

    try to fix neon database issues on vercel deploy

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,31 @@
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+import ws from "ws";
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${process.env.POSTGRES_PRISMA_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+const prisma = new PrismaClient({ adapter });
+
+/*if (process.env.NODE_ENV === "production") {
+  const neon = new Pool({
+    connectionString: process.env.POSTGRES_PRISMA_URL,
+  });
+  const adapter = new PrismaNeon(neon);
+  const prisma = new PrismaClient({ adapter });
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    const neon = new Pool({
+      connectionString: process.env.POSTGRES_PRISMA_URL,
+    });
+    const adapter = new PrismaNeon(neon);
+    global.prisma = new PrismaClient({ adapter });
   }
   prisma = global.prisma;
-}
+}*/
 
 export default prisma;
