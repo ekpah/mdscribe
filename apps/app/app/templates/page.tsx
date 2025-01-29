e5b8e829cb428de56eda59d9c4373122138f98d3
ekpah <nils.hapke@we-mail.de>
2025-01-29 17:25:39 +0100

    remove unnecessary database call from templates/page.tsx

--- apps/app/app/templates/page.tsx
+++ apps/app/app/templates/page.tsx
@@ -1,19 +1,8 @@
-import { database } from '@repo/database';
 import Link from 'next/link';
 import ContentSection from './[id]/_components/ContentSection';
 import { NavActions } from './[id]/_components/NavActions';
 
-export default async function TemplatesPage() {
-  const templates = await database.template.findMany({
-    include: {
-      author: true,
-      favouriteOf: true,
-    },
-    orderBy: {
-      updatedAt: 'desc',
-    },
-  });
-
+export default function TemplatesPage() {
   return (
     <div className="flex h-full w-full flex-col">
       <div className="flex h-10 items-center justify-between gap-2">
