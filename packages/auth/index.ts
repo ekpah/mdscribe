7e0f761b556670fe49d8279e4dd14c1c39097d40
ekpah <nils.hapke@we-mail.de>
2025-01-29 16:46:46 +0100

    add cookie caching to better-auth

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -6,9 +6,18 @@ import { betterAuth } from 'better-auth';
 
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 export const auth = betterAuth({
+  // sets the Better-Auth database adapter to Prisma with the PostgreSQL provider
   database: prismaAdapter(database, {
     provider: 'postgresql', // or "mysql", "postgresql", ...etc
   }),
+  // enables cookie caching for better-auth sessions
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // Cache duration in seconds
+    },
+  },
+  // enables login with the email and password flow
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: true,
