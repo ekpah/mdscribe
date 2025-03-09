d1d15bfdad5bcfd7632fea923ca9cbffc7fc7933
ekpah <nils.hapke@we-mail.de>
2025-03-09 23:10:54 +0100

    fix env in turbo

--- apps/app/lib/auth-client.ts
+++ apps/app/lib/auth-client.ts
@@ -1,10 +1,10 @@
 import { stripeClient } from '@better-auth/stripe/client';
+import { env } from '@repo/env';
 import { inferAdditionalFields } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 import type { auth } from '../auth.ts';
-
 export const authClient = createAuthClient({
-  baseURL: process.env.BASE_URL,
+  baseURL: env.BASE_URL as string,
   plugins: [
     inferAdditionalFields<typeof auth>(),
     // stripe plugin for subscription management
