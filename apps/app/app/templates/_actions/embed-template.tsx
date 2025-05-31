92145fff6c584147b993ae554fabfc57f8b7cb16
ekpah <nils.hapke@we-mail.de>
2025-05-31 14:53:13 +0200

    add langfuse to embedding

--- apps/app/app/templates/_actions/embed-template.tsx
+++ apps/app/app/templates/_actions/embed-template.tsx
@@ -1,13 +1,24 @@
 'use server';
+import { auth } from '@/auth';
 import { embed } from 'ai';
+import { headers } from 'next/headers';
 import { voyage } from 'voyage-ai-provider';
 
 export const generateEmbeddings = async (
   content: string
 ): Promise<{ embedding: number[]; content: string }> => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
   const { embedding } = await embed({
     model: voyage.textEmbeddingModel('voyage-3-large'),
     value: content,
+    experimental_telemetry: {
+      isEnabled: true,
+      metadata: {
+        userId: session?.user?.id || 'unknown',
+      },
+    },
   });
   return { embedding, content };
 };
