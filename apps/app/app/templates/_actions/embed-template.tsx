421ac9fb01457ad7436f0bb903796b455a234e89
ekpah <nils.hapke@we-mail.de>
2025-07-28 17:38:03 +0200

    fix another thing

--- apps/app/app/templates/_actions/embed-template.tsx
+++ apps/app/app/templates/_actions/embed-template.tsx
@@ -1,8 +1,11 @@
 'use server';
-import { auth } from '@/auth';
 import { embed } from 'ai';
 import { headers } from 'next/headers';
 import { voyage } from 'voyage-ai-provider';
+import { VoyageAIClient } from 'voyageai';
+import { auth } from '@/auth';
+
+const client = new VoyageAIClient({ apiKey: 'VOYAGE_API_KEY' });
 
 export const generateEmbeddings = async (
   content: string,
@@ -18,18 +21,25 @@ category: ${category}
 ---
 
 ${content}`;
-  const { embedding } = await embed({
-    model: voyage.textEmbeddingModel('voyage-3-large', {
-      inputType: 'document',
-    }),
-    value: contentWithMetadata,
-    experimental_telemetry: {
-      isEnabled: true,
-      metadata: {
-        userId: session?.user?.id || 'unknown',
-        value: contentWithMetadata,
-      },
-    },
-  });
+  const embedding = await client
+    .embed({
+      input: contentWithMetadata,
+      model: 'voyage-3-large',
+    })
+    .then((res) => res.data?.[0].embedding ?? []);
+
+  // await embed({
+  //   model: voyage.textEmbeddingModel('voyage-3-large', {
+  //     inputType: 'document',
+  //   }),
+  //   value: contentWithMetadata,
+  //   experimental_telemetry: {
+  //     isEnabled: true,
+  //     metadata: {
+  //       userId: session?.user?.id || 'unknown',
+  //       value: contentWithMetadata,
+  //     },
+  //   },
+  // });
   return { embedding };
 };
